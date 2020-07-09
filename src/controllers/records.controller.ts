4df26484dcf431a414bf434d3795f876158fa00e
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-07-09 19:02:57 +0100

    updated create record controller

--- src/controllers/records.controller.ts
+++ src/controllers/records.controller.ts
@@ -10,7 +10,7 @@ const createRecord = (req, res) => {
       return res.sendStatus(403)
     } else {
       const { insulin, glucose, dateTime, medication } = req.body
-      await prisma.records.create({
+      const createRecord = await prisma.records.create({
         data: {
           insulin: insulin,
           glucose: glucose,
@@ -19,7 +19,7 @@ const createRecord = (req, res) => {
           User: { connect: { id: cred.user.id } }
         }
       })
-      return res.sendStatus(201)
+      return res.json(createRecord)
     }
   })
 }
